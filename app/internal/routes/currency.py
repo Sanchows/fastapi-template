6a99bdb69607563b0e1f58eb3539b701b5a7a44e
Alexander Sachenko <sanyasach@mail.ru>
2023-02-17 17:41:22 +0300

    Some optimization

--- app/internal/routes/currency.py
+++ app/internal/routes/currency.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 from app.pkg.redis_tools.tools import RedisTools
 
 
@@ -6,10 +6,9 @@ router = APIRouter(prefix="/api/v1/currency")
 
 
 @router.get("/{pair}")
-async def get_currency_pair(
-    pair: str, keys: list = Depends(RedisTools.get_keys)
-):
-    if pair not in [s for s in keys]:
+async def get_currency_pair(pair: str):
+    price = await RedisTools.get_pair(pair)
+    if not price:
         return {"error": "This pair doesn't exists"}
 
-    return {"pair": pair, "price": await RedisTools.get_pair(pair)}
+    return {"pair": pair, "price": price}
